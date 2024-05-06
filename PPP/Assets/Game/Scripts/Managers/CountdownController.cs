201ccf5b5dfa79f82e3b9a730404336bf50a00ab
Jessica <jduquedeveloper@gmail.com>
2024-05-05 22:12:20 -0300

    Blessings screen

--- PPP/Assets/Game/Scripts/Managers/CountdownController.cs
+++ PPP/Assets/Game/Scripts/Managers/CountdownController.cs
@@ -18,6 +18,11 @@ public class CountdownController : Singleton<CountdownController>
         
     }
 
+    private void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
     #region Start Countdown
     public void StartCountdown()
     {
